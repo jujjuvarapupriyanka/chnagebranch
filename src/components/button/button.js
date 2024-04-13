f7e788c28be97f7d4c1cb325911d9f3d3f86c995
jujjuvarapupriyanka <jujjuvarapu.priyanka051@gmail.com>
2024-04-13 11:48:51 -0700

    done

---
+++ src/components/button/button.js
@@ -0,0 +1,6 @@
+function Button (){
+    return(
+        <button>submit</button>
+    )
+}
+export default Button;
---
+++ src/components/images/image.js
@@ -0,0 +1,6 @@
+function Image (){
+    return(
+        <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="" weight={150} height={150}/>
+    )
+}
+export default Image;
